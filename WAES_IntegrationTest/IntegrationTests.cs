98f078fd485fc4316ccaea47e1696286da801036
Jean Carlo <jean.carlo.jc1@gmail.com>
2018-03-06 17:40:53 -0300

    Added Integration test project and also started develop some tests

---
+++ WAES_IntegrationTest/IntegrationTests.cs
@@ -0,0 +1,135 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WAES_Test.Models;
+
+namespace WAES_IntegrationTest
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+        #region Constants
+        private const string item1 = "ew0KICAgICJuYW1lIjoiSm9obiIsDQogICAgImFnZSI6MzAsDQogICAgImNhcnMiOiBbDQogICAgICAgIHsgIm5hbWUiOiJGb3JkIiwgIm1vZGVscyI6WyAiRmllc3RhIiwgIkZvY3VzIiwgIk11c3RhbmciIF0gfSwNCiAgICAgICAgeyAibmFtZSI6IkJNVyIsICJtb2RlbHMiOlsgIjMyMCIsICJYMyIsICJYNSIgXSB9LA0KICAgICAgICB7ICJuYW1lIjoiRmlhdCIsICJtb2RlbHMiOlsgIjUwMCIsICJQYW5kYSIgXSB9DQogICAgXQ0KIH0=";
+        private const string item2 = "ew0KICAgICJuYW1lIjoiSmVhbiIsDQogICAgImFnZSI6MjYsDQogICAgImNhcnMiOiBbDQogICAgICAgIHsgIm5hbWUiOiJGb3JkIiwgIm1vZGVscyI6WyAiRmllc3RhIiwgIkZvY3VzIiwgIk11c3RhbmciIF0gfSwNCiAgICAgICAgeyAibmFtZSI6IkJNVyIsICJtb2RlbHMiOlsgIjMyMCIsICJYMyIsICJYNSIgXSB9LA0KICAgICAgICB7ICJuYW1lIjoiRmlhdCIsICJtb2RlbHMiOlsgIjUwMCIsICJQYW5kYSIgXSB9DQogICAgXQ0KIH0=";
+        private const string item3 = "ew0KCSJhZ2UiOjI2LA0KCSJuYW1lIjoiSmVhbiIsDQoJIm1lc3NhZ2UiOiAiV0FFUyINCn0=";
+        private const string baseURL = "http://waestestjean.azurewebsites.net/v1/diff";
+        private const string left = "left";
+        private const string right = "right";
+        private const string id = "1";
+        #endregion
+
+        static HttpClient client = new HttpClient();
+
+        [Test]
+        public async Task ValidateLeftEndpointReturnsOKwhenSuccess()
+        {
+            //Arrange
+            var content = new StringContent(item3, UnicodeEncoding.UTF8, "application/json");
+
+            //Act
+            HttpResponseMessage response = await client.PostAsync(
+                string.Format("{0}/{1}/{2}", baseURL, id, left), content);
+            response.EnsureSuccessStatusCode();
+
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task ValidateRightEndpointReturnsOKwhenSuccess()
+        {
+            //Arrange
+            var content = new StringContent(item3, UnicodeEncoding.UTF8, "application/json");
+            
+            //Act
+            HttpResponseMessage response = await client.PostAsync(
+                string.Format("{0}/{1}/{2}", baseURL, id, right), content);
+            response.EnsureSuccessStatusCode();
+
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task ValidateDiffEndpointSameData()
+        {
+            //Arrange
+            var content = new StringContent(item3, UnicodeEncoding.UTF8, "application/json");
+            HttpResponseMessage responseLeft = await client.PostAsync(
+                string.Format("{0}/{1}/{2}", baseURL, id, left), content);
+            responseLeft.EnsureSuccessStatusCode();
+            HttpResponseMessage responseRight = await client.PostAsync(
+                string.Format("{0}/{1}/{2}", baseURL, id, right), content);
+            responseRight.EnsureSuccessStatusCode();
+
+            // Act
+            ResultDiff resultDiff = null;
+            HttpResponseMessage response = await client.GetAsync(string.Format("{0}/{1}", baseURL, id));
+            if (response.IsSuccessStatusCode)
+            {
+                resultDiff = await response.Content.ReadAsAsync<ResultDiff>();
+            }
+
+            // Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(resultDiff.AreEqual);
+        }
+
+        [Test]
+        public async Task ValidateDiffEndpointSameSizeDifferentData()
+        {
+            //Arrange
+            var contentLeft = new StringContent(item1, UnicodeEncoding.UTF8, "application/json");
+            var contentRight = new StringContent(item2, UnicodeEncoding.UTF8, "application/json");
+            HttpResponseMessage responseLeft = await client.PostAsync(
+                string.Format("{0}/{1}/{2}", baseURL, id, left), contentLeft);
+            responseLeft.EnsureSuccessStatusCode();
+            HttpResponseMessage responseRight = await client.PostAsync(
+                string.Format("{0}/{1}/{2}", baseURL, id, right), contentRight);
+            responseRight.EnsureSuccessStatusCode();
+
+            // Act
+            ResultDiff resultDiff = null;
+            HttpResponseMessage response = await client.GetAsync(string.Format("{0}/{1}", baseURL, id));
+            if (response.IsSuccessStatusCode)
+            {
+                resultDiff = await response.Content.ReadAsAsync<ResultDiff>();
+            }
+
+            // Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Assert.IsTrue(resultDiff.SameSize);
+            Assert.IsFalse(resultDiff.AreEqual);
+        }
+
+        [Test]
+        public async Task ValidateDiffEndpointDifferentSizeAndData()
+        {
+            //Arrange
+            var contentLeft = new StringContent(item1, UnicodeEncoding.UTF8, "application/json");
+            var contentRight = new StringContent(item3, UnicodeEncoding.UTF8, "application/json");
+            HttpResponseMessage responseLeft = await client.PostAsync(
+                string.Format("{0}/{1}/{2}", baseURL, id, left), contentLeft);
+            responseLeft.EnsureSuccessStatusCode();
+            HttpResponseMessage responseRight = await client.PostAsync(
+                string.Format("{0}/{1}/{2}", baseURL, id, right), contentRight);
+            responseRight.EnsureSuccessStatusCode();
+
+            // Act
+            ResultDiff resultDiff = null;
+            HttpResponseMessage response = await client.GetAsync(string.Format("{0}/{1}", baseURL, id));
+            if (response.IsSuccessStatusCode)
+            {
+                resultDiff = await response.Content.ReadAsAsync<ResultDiff>();
+            }
+
+            // Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Assert.IsFalse(resultDiff.SameSize);
+            Assert.IsFalse(resultDiff.AreEqual);
+        }
+    }
+}
---
+++ WAES_IntegrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WAES_IntegrationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WAES_IntegrationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9282d0be-7dbb-4d92-b3d9-e0a0ab9157b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
